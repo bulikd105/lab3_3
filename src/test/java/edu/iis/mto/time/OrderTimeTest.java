e8dbd20339558908b9b1d534a6eeedd50c86db22
bulikd105 <bulikd105@gmail.com>
2016-06-05 19:29:25 +0200

    Stworzenie klasy do testów

---
+++ src/test/java/edu/iis/mto/time/OrderTimeTest.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.time;
+
+import edu.iis.mto.time.Order;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+public class OrderTimeTest 
+{
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
